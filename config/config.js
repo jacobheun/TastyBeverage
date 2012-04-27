e762116d3b2b435cff6286e6ec46df9bdab92e04
Nick Crohn <ncrohn@me.com>
2012-04-26 20:41:16 -0700

    Initial setup

---
+++ config/config.js
@@ -0,0 +1,8 @@
+{
+  "port": "6050",
+  "database": {
+    "host": "mongodb://flame.mongohq.com:27042/tastybeverage",
+    "username": "tastytasty",
+    "password": "swathuswAg8mAk5pupraTraJatudrurezubras3t"
+  }
+}
