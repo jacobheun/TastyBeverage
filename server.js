a106936d6c9ddf1b6453ffdd2237b2479ccddc41
Nick Crohn <ncrohn@me.com>
2012-05-03 22:11:52 -0700

    Fixed rouets and logging

--- server.js
+++ server.js
@@ -1,49 +1,40 @@
 var flatiron = require('flatiron'),
-path = require('path'),
-routes = require('./lib/plugins/routes'),
-handlebarsPlugin = require('./lib/plugins/handlebars'),
-connect = require('connect'),
-expressUglify = require('express-uglify'),
-pkg = require('./package.json'),
-app = flatiron.app;
+    path = require('path'),
+    routes = require('./lib/plugins/routes'),
+    connect = require('connect'),
+    expressUglify = require('express-uglify'),
+    pkg = require('./package.json'),
+    app = flatiron.app;
 
 app.config.file({ file: path.join(__dirname, 'config', 'config.json') });
 
 var port = app.config.get('port');
 
 app.use(flatiron.plugins.http, {
+
           before: [
             connect.static(__dirname + '/public', {maxAge: 86400000}),
             connect.staticCache()
           ],
 
-          after: [
-
-          ],
+          after: [],
 
           onError: function(err) {
-
             if(err) {
               this.res.writeHead(404, { "Content-Type": "text/html" });
-              this.res.end(app.render('404'));
+              this.res.end('404');
             }
           }
         });
 
-app.use(handlebarsPlugin, {
-          templates: __dirname + "/templates",
-          defaultLayout: 'layout',
-          blockHelpers: require('./lib/blockHelpers'),
-          cacheTemplates: true
-        });
 app.use(routes);
 
 app.start(port,
   function(err) {
     if(err) throw err;
-    app.log.info("      name :", pkg.name);
-    app.log.info("   version :", pkg.version);
-    app.log.info("started at :", Date());
-    app.log.info("   on port :", port);
-    app.log.info("   in mode :", app.env);
+    app.log.info("      name :".blue, pkg.name.grey);
+    app.log.info("   version :".blue, pkg.version.grey);
+    app.log.info("started at :".blue, Date().grey);
+    app.log.info("   on port :".blue, port.grey);
+    app.log.info("   in mode :".blue, app.env.grey);
   });
